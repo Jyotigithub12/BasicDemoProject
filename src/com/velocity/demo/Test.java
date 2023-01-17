833e4904e7695c392207585e1d1d181f264df490
Jyoti <jyotidesai12298@gmail.com>
2023-01-17 22:02:18 +0530

    message

---
+++ src/com/velocity/demo/Test.java
@@ -0,0 +1,16 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<String> list= new ArrayList<String> ();
+		list.add("Jyoti");
+		list.add("Tanaji");
+		list.add("Desai");
+		System.out.println(list);
+
+	}
+
+}
